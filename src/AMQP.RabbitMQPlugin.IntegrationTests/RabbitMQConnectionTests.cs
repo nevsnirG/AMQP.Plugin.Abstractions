f79e7c6fd1a8b2399b60b832d16b7d4f64a6db04
nevsnirG <14148970+nevsnirG@users.noreply.github.com>
2020-03-29 14:37:26 +0200

    Added RabbitMQPlugin.IntegrationTests project.

---
+++ src/AMQP.RabbitMQPlugin.IntegrationTests/RabbitMQConnectionTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AMQP.RabbitMQPlugin.IntegrationTests
+{
+    public class RabbitMQConnectionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

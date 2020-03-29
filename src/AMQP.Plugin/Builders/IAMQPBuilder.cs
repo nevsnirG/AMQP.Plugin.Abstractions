6861799281837e16ea9f9d53addf0f26fdf65ded
nevsnirG <14148970+nevsnirG@users.noreply.github.com>
2020-03-29 18:49:51 +0200

    Added all comments in the AMQP.Plugin library.

--- src/AMQP.Plugin/Builders/IAMQPBuilder.cs
+++ src/AMQP.Plugin/Builders/IAMQPBuilder.cs
@@ -2,11 +2,25 @@
 
 namespace AMQP.Plugin.Builders
 {
+    /// <summary>
+    /// Represents a fluid builder pattern for building and configuring AMQP implementations.
+    /// </summary>
     public interface IAMQPBuilder
     {
+        /// <summary>
+        /// The connection string to use when connecting to the message broker.
+        /// </summary>
         string ConnectionString { get; }
+        /// <summary>
+        /// The service collection to register the AMQP implementation and its dependencies to.
+        /// </summary>
         IServiceCollection Services { get; }
 
+        /// <summary>
+        /// Set the connection string for the message broker.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns>The <see cref="IAMQPBuilder"/> instance.</returns>
         IAMQPBuilder SetConnectionString(string connectionString);
     }
 }
--- src/AMQP.Plugin/Exceptions/BrokerUnreachableException.cs
+++ src/AMQP.Plugin/Exceptions/BrokerUnreachableException.cs
@@ -2,17 +2,32 @@
 
 namespace AMQP.Plugin.Exceptions
 {
+    /// <summary>
+    /// The exception that is thrown when a message broker could not be reached.
+    /// </summary>
     [Serializable]
     public sealed class BrokerUnreachableException : Exception
     {
+        /// <summary>
+        /// Initialize the <see cref="BrokerUnreachableException"/>.
+        /// </summary>
         public BrokerUnreachableException() : this("The message broker could not be reached.")
         {
         }
 
+        /// <summary>
+        /// Initialize the <see cref="BrokerUnreachableException"/>.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
         public BrokerUnreachableException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Initialize the <see cref="BrokerUnreachableException"/>.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        /// <param name="innerException">The inner exception that occured when establishing a connection.</param>
         public BrokerUnreachableException(string message, Exception innerException) : base(message, innerException)
         {
         }
